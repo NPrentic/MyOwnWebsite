d9af6bcb5f408bc2fc0ea9f7d647f686c68c190f
Nemanja Prentic <npr@3gproxy.com>
2019-07-04 13:57:39 +0200

    Importing a theme

--- FreelanceWebsite/ClientApp/src/app/home/home.component.ts
+++ FreelanceWebsite/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+    model = {
+        left: true,
+        middle: false,
+        right: false
+    };
+
+    focus;
+    focus1;
+    constructor() { }
+
+    ngOnInit() {}
 }
